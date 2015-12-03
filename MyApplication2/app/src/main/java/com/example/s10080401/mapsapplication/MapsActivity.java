5e4e5ef8b8859bdc1d7f685a09b09dbac44f6047
AvikAhuja <avikahuja100@gmail.com>
2015-12-03 12:52:21 -0600

    added a picture of a puppy and named it puppieeees

--- MyApplication2/app/src/main/java/com/example/s10080401/mapsapplication/MapsActivity.java
+++ MyApplication2/app/src/main/java/com/example/s10080401/mapsapplication/MapsActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -48,10 +50,11 @@ public class MapsActivity extends FragmentActivity {
                     .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
-                setUpMap();
-            }
         }
+        setUpMap();
     }
+    }
+
 
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
@@ -60,6 +63,9 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(3, 4)).title("Marker")
+                .title("Puppieeees")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.emily)));
     }
 }
+
